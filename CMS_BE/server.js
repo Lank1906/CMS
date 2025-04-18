9d11dc04579596f64452c91a40bedd80e74f6063
Lank1906 <buuixuanhoangc@gmail.com>
2025-04-18 22:43:33 +0700

    fix: format sequelize, server get port from env

--- CMS_BE/server.js
+++ CMS_BE/server.js
@@ -2,22 +2,23 @@ import express from 'express';
 import dotenv from 'dotenv';
 import sequelize from './src/config/db.js';
 import cors from 'cors';
+
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   }),
 );
 
 app.get('/', (req, res) => {
-  res.status(200).send('Server running on http://localhost:8000');
+  res.status(200).send('Server running on ' + process.env.BACKEND_URL);
 });
 
 try {
   await sequelize.authenticate();
-  app.listen(8000);
+  app.listen(process.env.PORT || 4000);
 } catch {}
--- CMS_BE/src/config/db.js
+++ CMS_BE/src/config/db.js
@@ -3,11 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize({
   host: process.env.DB_HOST,
   dialect: 'mysql',
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  logging: false,
+  database: process.env.DB_DATABASE,
 });
 
 export default sequelize;
