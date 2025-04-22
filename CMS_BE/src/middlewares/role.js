ff3452ed01650a7c13a5bcf1441b6af4cf5cebbd
DangPhuc <dangphucvghy195@gmail.com>
2025-04-22 23:19:17 +0700

    fix:resolve comment

--- CMS_BE/src/middlewares/role.js
+++
@@ -1,7 +0,0 @@
-export const isAdmin = (req, res, next) => {
-  const user = req.user;
-  if (!user || user.role !== 'admin') {
-    return res.status(403).json({ message: 'Forbidden - Admins only' });
-  }
-  next();
-};
